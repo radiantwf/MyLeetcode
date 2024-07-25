8ddbd06d81fabfd7d7806cb82a5f2ad1d273c592
radiantwf <radiantwf@gmail.com>
2024-07-26 03:16:49 +0800

    1

--- 双周赛/135 - 20240720/3235 - 困难 - 网格图操作后的最大分数/main.go
+++ 双周赛/135 - 20240720/3235 - 困难 - 网格图操作后的最大分数/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println()
+	// grid = [[10,9,0,0,15],[7,1,0,8,0],[5,20,0,11,0],[0,0,0,1,2],[8,12,1,10,3]]
+	// 输出：94
+	grid := [][]int{
+		{10, 9, 0, 0, 15},
+		{7, 1, 0, 8, 0},
+		{5, 20, 0, 11, 0},
+		{0, 0, 0, 1, 2},
+		{8, 12, 1, 10, 3},
+	}
+	fmt.Println(maximumScore(grid))
+}
+func maximumScore(grid [][]int) int64 {
+	return 0
 }
